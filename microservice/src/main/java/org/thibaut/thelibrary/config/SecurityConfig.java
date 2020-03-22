0dd27af40e5716284453c7bcd5caee1c607397a0
thibaut54 <john_milton100@hotmail.com>
2020-03-22 03:10:20 +0100

    config <microservice> cors still not working...

--- microservice/src/main/java/org/thibaut/thelibrary/config/SecurityConfig.java
+++ microservice/src/main/java/org/thibaut/thelibrary/config/SecurityConfig.java
@@ -7,12 +7,16 @@ import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @KeycloakConfiguration
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
@@ -72,10 +76,23 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     @Override
     protected void configure( HttpSecurity http) throws Exception {
         super.configure(http);
-        http.cors(  ).and()
+        http.cors(  ).configurationSource( configurationSource() ).and()
             .authorizeRequests()
             .antMatchers("/api/books").hasAnyRole("user", "admin")
 //            .antMatchers("/manager").hasRole("Librarian")
             .anyRequest().permitAll();
     }
+
+		private CorsConfigurationSource configurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.setAllowCredentials(true);
+		config.addAllowedHeader("X-Requested-With");
+		config.addAllowedHeader("Content-Type");
+		config.addAllowedMethod( HttpMethod.POST);
+		config.addAllowedMethod( HttpMethod.GET);
+		source.registerCorsConfiguration("/logout", config);
+		return source;
+	}
 }
