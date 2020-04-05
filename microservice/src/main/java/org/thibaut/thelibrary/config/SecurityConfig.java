5c82d470750402fc424311c9a30764f48d614a9b
thibaut54 <john_milton100@hotmail.com>
2020-04-05 02:31:46 +0200

    security remove unsed line

--- microservice/src/main/java/org/thibaut/thelibrary/config/SecurityConfig.java
+++ microservice/src/main/java/org/thibaut/thelibrary/config/SecurityConfig.java
@@ -24,7 +24,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// Validate tokens through configured OpenID Provider
-//		http.cors().and().authorizeRequests(  ).anyRequest().permitAll();
 		http.cors().and().oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
 		http.authorizeRequests().antMatchers(HttpMethod.GET, "/actuator/**").permitAll();
 		http.authorizeRequests().antMatchers(HttpMethod.POST, "/actuator/**").permitAll();
